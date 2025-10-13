d3f306fa063dfea00ae2330e4becd90d95e3b12c
John Fil Roe Taboada <186065733+Taboada40@users.noreply.github.com>
2025-10-13 20:38:25 +0800

    File name change | Added loginpage | Added signupage | Added base homepage

--- Frontend/landing_page/templates/landingpage.jsx
+++ Frontend/landing_page/templates/landingpage.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "../assets/imgs/react.svg";
+import "../assets/styles/landingpage.css";
 
-function App() {
+function landingpage() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -22,7 +18,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>landingpage.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
@@ -32,4 +28,4 @@ function App() {
   )
 }
 
-export default App
+export default landingpage
