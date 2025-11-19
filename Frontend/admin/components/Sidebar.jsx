ab503769179320ce18bdb0ca0f168c05ceed3c40
theninanicole <villadareznn@gmail.com>
2025-11-20 07:53:18 +0800

    frontend(admin): change sidebar icons

---
+++ Frontend/admin/components/Sidebar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import userImg from "../assets/imgs/user.png";
+import chartImg from "../assets/imgs/chart.png";
+import bagImg from "../assets/imgs/bag.png"; // Assuming 'Manage' uses a bag/product icon
+import homeImg from "../assets/imgs/home.png"; // Assuming 'Dashboard' uses a home icon
+import { Link } from "react-router-dom";
+
+const Sidebar = () => (
+  <aside className="sidebar">
+    <a href="#" className="logo-admin">
+      <div className="logo-icon">PH</div>
+      <a href="/" className="landing">Pinoy Heritage</a>
+    </a>
+    <nav className="sidebar-menu">
+      <a href="#" className="menu-item">
+        <span className="menu-icon">
+          <img src={homeImg} alt="Dashboard" className="icon-img" /> {/* Changed to homeImg */}
+        </span>
+        <span>Dashboard</span>
+      </a>
+      <a href="#" className="menu-item">
+        <span className="menu-icon">
+          <img src={userImg} alt="Users" className="icon-img" /> {/* Changed to userImg */}
+        </span>
+        <span>Users</span>
+      </a>
+      <a href="#" className="menu-item active">
+        <span className="menu-icon">
+          <img src={bagImg} alt="Manage" className="icon-img" /> {/* Changed to bagImg */}
+        </span>
+        <span>Manage</span>
+      </a>
+      <a href="#" className="menu-item">
+        <span className="menu-icon">
+          <img src={chartImg} alt="Monitor" className="icon-img" /> {/* Changed to chartImg */}
+        </span>
+        <span>Monitor</span>
+      </a>
+    </nav>
+    <a href="#" className="menu-item logout-item">
+      {/* You didn't provide an image for logout, so I'll keep the door emoji or you can add one */}
+      <span className="menu-icon">🚪</span> 
+      <span>Logout</span>
+    </a>
+  </aside>
+);
+
+export default Sidebar;
