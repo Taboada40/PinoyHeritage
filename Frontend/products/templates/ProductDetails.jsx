c3acbf772917dbe90570bed5e36f01d829d692a6
theninanicole <villadareznn@gmail.com>
2025-11-20 07:46:28 +0800

    frontend(product): refined the styling and added more static products

--- Frontend/products/templates/ProductDetails.jsx
+++ Frontend/products/templates/ProductDetails.jsx
@@ -10,6 +10,10 @@ import barotSayaImg from "../assets/imgs/barotsaya.png";
 import filipinianaImg from "../assets/imgs/filipiniana.jpg";
 import malongImg from "../assets/imgs/malong.jpg";
 import arrowImg from "../assets/imgs/arrow-right.png";
+import salakotImg from "../assets/imgs/salakot.jpg"; 
+import camisaImg from "../assets/imgs/camisa.jpg";
+import bakyaImg from "../assets/imgs/bakya.jpg";
+import tnalakImg from "../assets/imgs/tnalak.jpg";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -138,6 +142,62 @@ export default function ProductDetails() {
         },
       ],
     },
+    {
+      id: 5,
+      name: "Salakot",
+      price: 350.00,
+      stock: 50,
+      rating: 4,
+      description: 'A traditional wide-brimmed hat usually made of rattan or reeds, used by farmers and iconic in Filipino culture.',
+      image: salakotImg,
+      sizes: ['Standard', 'Large'],
+      reviews: [
+        { id: 1, author: 'r***y', rating: 5, tags: ['Authentic', 'Sturdy'], date: '01 Nov 2025' },
+        { id: 2, author: 'k***l', rating: 3, tags: ['Good for displays'], date: '15 Oct 2025' }
+      ]
+    },
+    {
+      id: 6,
+      name: "Camisa de Chino",
+      price: 450.00,
+      stock: 120,
+      rating: 3,
+      description: 'A simple, collarless cotton shirt usually worn underneath the Barong Tagalog or as casual wear.',
+      image: camisaImg,
+      sizes: ['S', 'M', 'L', 'XL'],
+      reviews: [
+        { id: 1, author: 'j**n', rating: 3, tags: ['Thin fabric'], date: '10 Oct 2025' },
+        { id: 2, author: 'b**oy', rating: 4, tags: ['Breathable', 'Comfy'], date: '22 Sept 2025' }
+      ]
+    },
+    {
+      id: 7,
+      name: "Bakya",
+      price: 250.00,
+      stock: 65,
+      rating: 4,
+      description: 'Traditional Filipino wooden clogs with a strap, often made from native lightweight wood like santol or laniti.',
+      image: bakyaImg,
+      sizes: ['36', '37', '38', '39', '40'],
+      reviews: [
+        { id: 1, author: 'l**a', rating: 5, tags: ['Nostalgic', 'Cute'], date: '05 Nov 2025' },
+        { id: 2, author: 'g**r', rating: 4, tags: ['Hard to walk in but nice'], date: '20 Oct 2025' }
+      ]
+    },
+    {
+      id: 8,
+      name: "T'nalak Vest",
+      price: 1500.00,
+      stock: 15,
+      rating: 5,
+      description: 'A vest woven from T\'nalak fabric, a sacred cloth of the T\'boli people made from abaca fibers.',
+      image: tnalakImg,
+      sizes: ['S', 'M', 'L'],
+      reviews: [
+        { id: 1, author: 'art**luv', rating: 5, tags: ['Stunning patterns', 'Premium'], date: '12 Nov 2025' },
+        { id: 2, author: 'pinoy**pride', rating: 5, tags: ['Great craftsmanship'], date: '01 Oct 2025' }
+      ]
+    },
   ];
 
   const product = products.find((p) => p.id === parseInt(id));
@@ -161,7 +221,7 @@ export default function ProductDetails() {
 
   return (
     <div className="product-details-page">
-      <Header showNav={false} />
+      <Header showNav={true} />
 
       {/* Back Button */}
       <div className="details-header">
